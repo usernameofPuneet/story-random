a4c9142f901e91d97f197e26361564cfecfba481
Puneet Sharma <puneet5may@gmail.com>
2020-05-28 16:28:56 +0530

    added name and email fields, ready for first deploy

--- app.js
+++ app.js
@@ -6,15 +6,34 @@ const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://puneet:test123@cluster0-tae4f.mongodb.net/blogDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://puneet:test123@cluster0-tae4f.mongodb.net/blogDB", {useNewUrlParser: true, useUnifiedTopology: true}, function(err){
+  if(!err){
+    console.log("successfully connected to database");
+    
+  }
+})
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const homeStartingContent = "Welcome to our website. Here you can post any random story. It can be from your life or someone else's, it can be a true incident or just a product of your imagination, it can be something that prouds you or a confession, a funny incident or a heart break poem, you can post anything your name will be a secret so leave the tension of being in fame ot infamy. If you want to share what is in your mind just do it here. To publish your own story click on the 'COMPOSE' on the navbar. Stop thinking and start typing! All the best with your masterpiece."
+const aboutContent = "This site is built by Puneet Sharma, currently pursuing B.Tech in Computer Science from MNNIT, Allahabad. He was too bored during lockdown so he made this website. Please don't show your personal hatred against him in posts of this website. He would soon be adding new features for stories like login, signup, comments, like and dislike etc.";
+const contactContent = "If you want to contact me you can refer the following details";
 
 const postSchema = new mongoose.Schema({
-  title : String,
-  content : String
+  writerName: {
+    type: String,
+    required: [true, "Please specify a name"]
+  } ,
+  email: {
+    type: String,
+    required: [true, "Please specify an email!"]
+  } ,
+  title : {
+    type: String,
+    required: [true, "Please specify a title."]
+  } ,
+  content :{
+    type: String,
+    required: [true, "Please write something in your post."]
+  } 
 });
 
 const Post = mongoose.model("Post", postSchema);
@@ -66,9 +85,11 @@ app.post("/compose", function(req, res){
   // posts.push(post);
 
   const post = new Post ({
+    writerName: req.body.writerName,
+    email: req.body.emailId,
     title: req.body.postTitle,
     content: req.body.postBody
-  })
+  });
 
   post.save();
 
